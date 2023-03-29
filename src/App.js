32d49dde9a7b3c0d798c1d10348670ed30a33804
mayrang <pkss0626@naver.com>
2023-03-29 16:57:41 +0900

    router 관련 설정과 pages 추가, Auth api 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import AddProduct from "./pages/AddProduct";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Products from "./pages/Products";
+import Root from "./pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/products", element: <Products /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/product/add", element: <AddProduct /> },
+      { path: "/product/detail/:productId", element: <ProductDetail /> },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <RouterProvider router={router} />;
 }
 
 export default App;
