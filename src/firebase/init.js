3d1ab403db8c1b178012d494061bca56b52a5b36
mayrang <pkss0626@naver.com>
2023-03-29 15:06:52 +0900

    first commit

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQmU3nqAVuArDlIF_vsnYCqAnzshZs9Lg",
-  authDomain: "mayrang-shoppy.firebaseapp.com",
-  databaseURL: "https://mayrang-shoppy-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mayrang-shoppy",
-  storageBucket: "mayrang-shoppy.appspot.com",
-  messagingSenderId: "871859531754",
-  appId: "1:871859531754:web:4be57c6acfa5482aaf8c93",
-  measurementId: "G-2813BKNQ6D",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
