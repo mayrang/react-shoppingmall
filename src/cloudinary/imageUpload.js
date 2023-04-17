2713506ae409873fc7491a3b09d22fe390de9fd3
mayrang <pkss0626@naver.com>
2023-04-17 16:01:27 +0900

    add product image handle 추가

--- src/cloudinary/imageUpload.js
+++ src/cloudinary/imageUpload.js
@@ -1,22 +1,28 @@
 import axios from "axios";
 
 export const imageUpload = async (files) => {
-  const formData = new FormData();
-  formData.append("api_key", process.env.REACT_APP_CLOUDINARY_API_KEY);
+  try {
+    const formData = new FormData();
+    formData.append("api_key", process.env.REACT_APP_CLOUDINARY_API_KEY);
 
-  formData.append("timestamp", (Date.now() / 1000) | 0);
-  formData.append("file", files[0]);
+    formData.append("timestamp", (Date.now() / 1000) | 0);
+    formData.append("file", files[0]);
 
-  formData.append("upload_preset", "react-shoppingmall");
+    formData.append("upload_preset", "react-shoppingmall");
 
-  const config = {
-    header: {
-      "Content-Type": "multipart/form-data",
-    },
-  };
-  console.log(123);
-  const cloudName = process.env.REACT_APP_CLOUD_NAME;
-  const result = await axios.post(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, formData, config);
+    const config = {
+      header: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    console.log(123);
+    const cloudName = process.env.REACT_APP_CLOUD_NAME;
 
-  console.log(result.data.url);
+    const result = await axios.post(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, formData, config);
+
+    return result.data.url;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
 };
--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { imageUpload } from "../cloudinary/imageUpload";
 
 export default function AddProduct() {
+  const [imageUrl, setImageUrl] = useState(null);
+  const changeFile = async (e) => {
+    const url = await imageUpload(e.target.files);
+    setImageUrl(url);
+  };
+  const handleDelteImage = () => {
+    setImageUrl(null);
+  };
   return (
-    <div>
-      <form className="pt-20 flex flex-col items-center w-screen max-w-[1140px] mx-auto">
-        <input type="file" className="w-full " accept="image/*" />
-      </form>
-    </div>
+    <form className="pt-52 flex flex-col items-center w-screen max-w-[1140px] mx-auto">
+      <div className="w-full">
+        <label htmlFor="image">이미지</label>
+        <div className="border rounded-md p-2 w-full">
+          <div className="flex item-center justify-between">
+            <input type="file" className="w-1/2" accept="image/*" onChange={changeFile} />
+            {imageUrl && (
+              <button className=" px-2 py-1 bg-red-500 rounded text-white" onClick={handleDelteImage}>
+                삭제
+              </button>
+            )}
+          </div>
+          {imageUrl && <img src={imageUrl} className="mt-3" alt="쇼핑 이미지" />}
+        </div>
+      </div>
+    </form>
   );
 }
